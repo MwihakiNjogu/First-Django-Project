05fc8c9b9626beefbdc86162ce6b1d4118d69c31
Mwihaki Njogu <you@example.com>
2023-03-10 16:36:32 +0300

    my first commit

---
+++ MwihakiDjango/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+
+
+def homepage(request):
+    return render(request, 'home.html')
+def services(request):
+    return render(request, 'services.html')
+
+def product(request):
+    return render(request,'products.html')
+
+def contact(request):
+    return render(request,'contact.html')
+
+def blog(request):
+    return render(request,'blog.html')
+
+def about(request):
+    return render(request,'about.html')
